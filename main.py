90b4f1f33371cd6d8efaf0a0d511a61ba8352d55
Maham93-Python <mustufahosain5@gmail.com>
2026-09-23 14:54:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+name = input("Enter your name: ")
+marks = float(input("Enter your marks: "))
+if marks >= 80:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 50:
+    grade = "D"
+else:
+    grade = "F"
 
+print()
+print("Student:", name)
+print("Marks:", marks)
+print("Grade:", grade)
